92e89d106cbd21db8b8fba0f3f9ad06961369832
EvilKhaosKat <evilkhaoskat@gmail.com>
2016-09-28 23:35:20 +0400

    Termbox added, some experiments with output

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/nsf/termbox-go"
+	"time"
+)
+
+type GameMap struct {
+	CellAutoMap [][]bool
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	//gameMap := initGameMap(termbox.Size())
+	//fmt.Println(gameMap)
+	termbox.SetCell(5, 10, '⏣', termbox.ColorWhite, termbox.ColorBlack)
+	termbox.SetCell(1, 2, '⏺', termbox.ColorWhite, termbox.ColorBlack)
+	termbox.SetCell(10, 5, '⏹', termbox.ColorWhite, termbox.ColorBlack)
+	termbox.Flush()
+
+	time.Sleep(time.Second*3)
+}
+
+func initGameMap(width, height int) GameMap {
+	cellAutoMap := make([][]bool, height)
+
+	for i := 0; i < height; i++ {
+		cellAutoMap[i] = make([]bool, width)
+	}
+
+	return GameMap{cellAutoMap}
+}

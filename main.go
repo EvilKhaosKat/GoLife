e7d134964067d2c367e98ed466e19b600f803524
EvilKhaosKat <evilkhaoskat@gmail.com>
2016-10-03 22:28:12 +0400

    Interim changes

--- main.go
+++ main.go
@@ -5,10 +5,34 @@ import (
 	// "github.com/gdamore/tcell"
 	// "time"
 	// "os"
+	"fmt"
 )
 
+const DELAY_MS = 200
+
 type GameMap struct {
-	CellAutoMap [][]bool
+	cellMap      [][]bool
+	changeEvents []ChangeEvent
+
+}
+
+func (gameMap *GameMap) getSize() (int, int) {
+	height := len(gameMap)
+	if height == 0 {
+		panic("Height must not be 0")
+	}
+
+	width := len(gameMap[0])
+	if width == 0 {
+		panic("Width must not be 0")
+	}
+
+	return height, width
+}
+
+type ChangeEvent struct {
+	x,y int
+	newValue bool
 }
 
 func main() {
@@ -36,20 +60,26 @@ func main() {
 }
 
 func launchLife() {
-	//gameMap := initGameMap(termbox.Size())
-	//fmt.Println(gameMap)
+	gameMap := initGameMap(termbox.Size())
+	fmt.Println(gameMap)
+
+
 	termbox.SetCell(5, 10, '⏣', termbox.ColorWhite, termbox.ColorBlack)
 	termbox.SetCell(1, 2, '⏺', termbox.ColorWhite, termbox.ColorBlack)
 	termbox.SetCell(10, 5, '⏹', termbox.ColorWhite, termbox.ColorBlack)
 	termbox.Flush()
 }
 
-func initGameMap(width, height int) GameMap {
+func initGameMap(width, height int) *GameMap {
 	cellAutoMap := make([][]bool, height)
 
 	for i := 0; i < height; i++ {
 		cellAutoMap[i] = make([]bool, width)
 	}
 
-	return GameMap{cellAutoMap}
+	return &GameMap{cellMap: cellAutoMap}
+}
+
+func fillMapRandomValues(gameMap *GameMap) {
+
 }
